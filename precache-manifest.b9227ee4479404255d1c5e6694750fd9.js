bd41f3303641fd9ed81e51ac67272ca3d7ff0b2f
Siddhesh Pisal <siddheshpisal@MUMCAP0074.local>
2019-10-16 12:17:56 +0530

    Updates

---
+++ precache-manifest.b9227ee4479404255d1c5e6694750fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03361d6d612d507c2d83e1ae35d5613e",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "439f9cd804a265b0774e",
+    "url": "/resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2460792d93115cb05779",
+    "url": "/resume/static/js/2.874e4b01.chunk.js"
+  },
+  {
+    "revision": "439f9cd804a265b0774e",
+    "url": "/resume/static/js/main.7036d77d.chunk.js"
+  },
+  {
+    "revision": "5463ca49598b2253796f",
+    "url": "/resume/static/js/runtime-main.47192d09.js"
+  }
+]);
